3dcf5d3f368721cfa36e8def0d4208fc6ab2ee1a
secimpalut <60846960+secimpalut@users.noreply.github.com>
2020-11-08 19:32:49 +0200

    Fifth commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -14,6 +14,7 @@ public class FirstClass {
 	
 
 		System.out.println("Here is the conflict we have from remote master branch");
+		System.out.println("Here is another conflict from remote master branch");
 
 	}
 
