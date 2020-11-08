65019e4787168fa651cdf5ddee0cca4f5ebc69d4
secimp <secimpalut28@gmail.com>
2020-11-08 18:12:27 +0200

    Third commit local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First Commit from local master branch");
 		System.out.println("Second Commit from local master");
+		System.out.println("Third Commit from local master");
 	}
 
 }
